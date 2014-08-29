9388c10f6a9fbc1c1a66b707bd99a3a3297de7bd
hiraya.shingo <hirayashingo@HL00065.local>
2014-08-29 13:20:04 +0900

    first commit

--- GoogleMapsSample/ViewController.m
+++ GoogleMapsSample/ViewController.m
@@ -9,14 +9,31 @@
 #import "ViewController.h"
 #import <GoogleMaps/GoogleMaps.h>
 
-static NSInteger const markerIdClassmethod      = 1;
-static NSInteger const markerIdYodobashi        = 2;
-static NSInteger const markerIdAkihabaraStation = 3;
-static NSInteger const markerIdDenkigai         = 4;
+/**
+ *  マーカーの表示状態
+ */
+typedef NS_ENUM(NSInteger, markerState) {
+    /**
+     *  非表示
+     */
+    markerStateHidden = 1,
+    /**
+     *  表示中
+     */
+    markerStateDisplay,
+};
 
 @interface ViewController () <GMSMapViewDelegate>
 
+/**
+ *  `GMSMapView`
+ */
 @property (strong, nonatomic) GMSMapView *mapView;
+
+/**
+ *  GMSMarkerの配列
+ */
+@property (strong, nonatomic) NSArray *markers;
 
 @end
 
@@ -46,75 +63,125 @@ static NSInteger const markerIdDenkigai         = 4;
     self.mapView.delegate = self;
     
     self.view = self.mapView;
-    
-    [self addMarkers];
-}
-
-#pragma mark - private methods
-
-- (void)addMarkers
-{
-    // (6) デフォルトのマーカー
-    GMSMarker *yodobashiMarker = [[GMSMarker alloc] init];
-    yodobashiMarker.title = @"ヨドバシAkiba";
-    yodobashiMarker.snippet = @"千代田区神田花岡町１−１";
-    yodobashiMarker.position = CLLocationCoordinate2DMake(35.698852, 139.774761);
-    yodobashiMarker.map = self.mapView;
-    yodobashiMarker.userData = [NSNumber numberWithInteger:markerIdYodobashi];
-    
-    // (7) マーカーのアイコンをカスタム画像にする場合
-    GMSMarker *akihabaraStationMarker = [[GMSMarker alloc] init];
-    akihabaraStationMarker.title = @"秋葉原駅";
-    akihabaraStationMarker.snippet = @"千代田区外神田1丁目";
-    akihabaraStationMarker.position = CLLocationCoordinate2DMake(35.698404, 139.773001);
-    akihabaraStationMarker.map = self.mapView;
-    akihabaraStationMarker.icon = [UIImage imageNamed:@"pin"];
-    akihabaraStationMarker.userData = [NSNumber numberWithInteger:markerIdAkihabaraStation];
-    
-    // (8) マーカーをタップした時に表示されるウィンドウ自体をカスタムビューにする場合
-    GMSMarker *classmethodMarker = [[GMSMarker alloc] init];
-    classmethodMarker.title = @"クラスメソッド株式会社";
-    classmethodMarker.snippet = @"千代田区神田佐久間町1丁目11番地";
-    classmethodMarker.position = CLLocationCoordinate2DMake(35.697236, 139.774718);
-    classmethodMarker.map = self.mapView;
-    classmethodMarker.userData = [NSNumber numberWithInteger:markerIdClassmethod];
-    
-    // (9) マーカーをタップした時に表示されるウィンドウの中のビューをカスタムビューにする場合
-    GMSMarker *denkigaiMarker = [[GMSMarker alloc] init];
-    denkigaiMarker.title = @"電気街";
-    denkigaiMarker.snippet = @"千代田区外神田２丁目２−１６−２";
-    denkigaiMarker.position = CLLocationCoordinate2DMake(35.699649, 139.771419);
-    denkigaiMarker.map = self.mapView;
-    denkigaiMarker.userData = [NSNumber numberWithInteger:markerIdDenkigai];
 }
 
 #pragma mark - GMSMapViewDelegate methods
 
-// ウィンドウを返却する
-- (UIView *)mapView:(GMSMapView *)mapView markerInfoWindow:(GMSMarker *)marker
+- (void)mapView:(GMSMapView *)mapView idleAtCameraPosition:(GMSCameraPosition *)position
 {
-    NSInteger markerId = [marker.userData integerValue];
-    if (markerId == markerIdClassmethod) {
-        UIView *view = [[[NSBundle mainBundle] loadNibNamed:@"View"
-                                                      owner:self
-                                                    options:nil] lastObject];
-        return view;
-    } else {
-        return nil;
+    [self reloadMarkers];
+}
+
+#pragma mark - private methods
+
+/**
+ *  マーカーの表示を更新する
+ */
+- (void)reloadMarkers
+{
+    // 表示されているマップアイテムのうち、画面外になったマーカー : マーカー削除
+    // 非表示のマップアイテムのうち、画面内になったマーカー : マーカー追加
+    for (GMSMarker *marker in self.markers) {
+        if (marker.map) {
+            // 表示中Item
+            if ([self containsMarkerInMap:marker] == NO) {
+                // マーカーが領域外になった
+                marker.map = NO;
+                NSLog(@"hidden %@ marker", marker.title);
+            }
+        } else {
+            // 非表示のItem
+            if ([self containsMarkerInMap:marker] == YES) {
+                // マーカーが領域内になった
+                // フラグを表示に変更
+                marker.map = self.mapView;
+                NSLog(@"display %@ marker", marker.title);
+            }
+        }
     }
 }
 
-// ウィンドウ内のコンテンツビューを返却する
-- (UIView *)mapView:(GMSMapView *)mapView markerInfoContents:(GMSMarker *)marker
+/**
+ *  現在の可視領域のGMSCoordinateBoundsを返す
+ *
+ *  @return `GMSCoordinateBounds`
+ */
+- (GMSCoordinateBounds *)visiableCoordinateBounds
 {
-    NSInteger markerId = [marker.userData integerValue];
-    if (markerId == markerIdDenkigai) {
-        UIImage *image = [UIImage imageNamed:@"photo.jpg"];
-        UIImageView *imageView = [[UIImageView alloc]initWithImage:image];
-        return imageView;
-    } else {
-        return nil;
+    CLLocationCoordinate2D bottomLeftCoord = self.mapView.projection.visibleRegion.nearLeft;
+    CLLocationCoordinate2D topRightCoord = self.mapView.projection.visibleRegion.farRight;
+    
+    return [[GMSCoordinateBounds alloc]initWithCoordinate:topRightCoord coordinate:bottomLeftCoord];
+}
+
+/**
+ *  markerが地図の可視領域に存在するか
+ *
+ *  @param marker `GMSMarker`
+ *
+ *  @return markerが地図の可視領域に存在する場合YES、そうでなければNO
+ */
+- (BOOL)containsMarkerInMap:(GMSMarker *)marker
+{
+    return [self.visiableCoordinateBounds containsCoordinate:marker.position];
+}
+
+- (NSArray *)markers
+{
+    if (!_markers) {
+        GMSMarker *yodobashiMarker = [GMSMarker new];
+        yodobashiMarker.title = @"ヨドバシAkiba";
+        yodobashiMarker.position = CLLocationCoordinate2DMake(35.698852, 139.774761);
+        yodobashiMarker.userData = [NSNumber numberWithInteger:markerStateHidden];
+        
+        GMSMarker *akihabaraStationMarker = [GMSMarker new];
+        akihabaraStationMarker.title = @"秋葉原駅";
+        akihabaraStationMarker.position = CLLocationCoordinate2DMake(35.698404, 139.773001);
+        akihabaraStationMarker.userData = [NSNumber numberWithInteger:markerStateHidden];
+        
+        GMSMarker *classmethodMarker = [GMSMarker new];
+        classmethodMarker.title = @"クラスメソッド株式会社";
+        classmethodMarker.position = CLLocationCoordinate2DMake(35.697236, 139.774718);
+        classmethodMarker.userData = [NSNumber numberWithInteger:markerStateHidden];
+        
+        GMSMarker *denkigaiMarker = [GMSMarker new];
+        denkigaiMarker.title = @"電気街";
+        denkigaiMarker.position = CLLocationCoordinate2DMake(35.699649, 139.771419);
+        denkigaiMarker.userData = [NSNumber numberWithInteger:markerStateHidden];
+        
+        GMSMarker *kandaMarker = [GMSMarker new];
+        kandaMarker.title = @"神田駅";
+        kandaMarker.position = CLLocationCoordinate2DMake(35.691690, 139.770883);
+        kandaMarker.userData = [NSNumber numberWithInteger:markerStateHidden];
+        
+        GMSMarker *uenoMarker = [GMSMarker new];
+        uenoMarker.title = @"上野駅";
+        uenoMarker.position = CLLocationCoordinate2DMake(35.713768, 139.777254);
+        uenoMarker.userData = [NSNumber numberWithInteger:markerStateHidden];
+        
+        GMSMarker *ochanomizuMarker = [GMSMarker new];
+        ochanomizuMarker.title = @"御茶ノ水駅";
+        ochanomizuMarker.position = CLLocationCoordinate2DMake(35.699855, 139.763786);
+        ochanomizuMarker.userData = [NSNumber numberWithInteger:markerStateHidden];
+        
+        GMSMarker *asakusabashiMarker = [GMSMarker new];
+        asakusabashiMarker.title = @"浅草橋駅";
+        asakusabashiMarker.position = CLLocationCoordinate2DMake(35.697467, 139.785976);
+        asakusabashiMarker.userData = [NSNumber numberWithInteger:markerStateHidden];
+        
+        _markers = @[
+                     yodobashiMarker,
+                     akihabaraStationMarker,
+                     classmethodMarker,
+                     denkigaiMarker,
+                     kandaMarker,
+                     uenoMarker,
+                     ochanomizuMarker,
+                     asakusabashiMarker,
+                     ];
     }
+    
+    return _markers;
 }
 
 @end
